1d4e6bfbdc6c576f3632986dcb183b629a9372a5
niwanowa <be_girl@outlook.jp>
2018-07-16 20:26:50 +0900

    add change img function

--- cranes.js
+++ cranes.js
@@ -1,4 +1,24 @@
-function OnButtonClick() {
-	target = document.getElementById("output");
-	target.innerHTML = "鶴";
+var img = new Array();
+
+img[0] = new Image();
+img[0].src = "img/dog1.jpg";
+img[1] = new Image();
+img[1].src = "img/dog2.jpg";
+img[2] = new Image();
+img[2].src = "img/dog3.jpg";
+img[3] = new Image();
+img[3].src = "img/dog4.jpg";
+img[4] = new Image();
+img[4].src = "img/dog5.jpg";
+
+var photo_cnt = 0;
+
+function changeIMG(){
+  photo_cnt++;
+
+  if(photo_cnt == 5){
+    photo_cnt = 0;
+  }
+
+  document.getElementById("photos").src=img[photo_cnt].src;
 }
